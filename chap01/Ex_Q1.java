90c0189669cdbc182da5e3d75e92ae9aa330eaa0
suji5262 <suji5262@gmail.com>
2023-07-18 22:26:25 +0900

    :bulb: Ex_Q1 주석 수정

--- chap01/Ex_Q1.java
+++ chap01/Ex_Q1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ex_Q1 {
+public class Ex_Q1 { // 네 값의 최댓값을 구하라
 
     static int max4(int a, int b, int c, int d){ // a, b, c, d 최댓값을 구해 반환
         int max = a; // 최댓값
