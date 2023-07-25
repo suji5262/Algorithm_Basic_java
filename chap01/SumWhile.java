65506029749b01e55e684255d56f0e7b5c96a781
suji5262 <suji5262@gmail.com>
2023-07-25 12:40:17 +0900

    :sparkles: SumWhile file add

---
+++ chap01/SumWhile.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("1부터 n까지의 합을 구합니다.");
+        System.out.print("n값: ");
+        int n = sc.nextInt();
+
+        int sum = 0; // 합
+        int i = 1;
+
+        while (i <= n) { // i가 n 이하면 반복
+            sum += i; // sum 에 i를 더함
+            i++; // i값을 1만큼 증가시킴
+        }
+
+        System.out.println("1부터 " + n + "까지의 합은 " + sum + "입니다.");
+    }
+}
